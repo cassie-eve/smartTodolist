f6fc8d3ce1a6c2b319554aed0a1003f747db42bf
Gaurav Mahant <thegau22@gmail.com>
2023-01-06 16:16:22 -0700

    change date format to YYYY/MM/DD

--- db/seeds/seeds_tasks.sql
+++ db/seeds/seeds_tasks.sql
@@ -1,5 +1,5 @@
 INSERT INTO tasks (id, name, category, due_date, completed, priority, users_id) VALUES
-(1, 'Harry Potter', 'books', '14/01/2023', 'true', 5, 1),
-(2, 'Tacofino', 'restaurants', '17/02/2023', 'false', 3, 2),
-(3, 'Avatar', 'movies', '22/01/2023', 'false', 7, 3),
-(4, 'Dish soap', 'products', '06/01/2023', 'true', 10, 4);
+(1, 'Harry Potter', 'books', '2023/01/14', 'true', 5, 1),
+(2, 'Tacofino', 'restaurants', '2023/02/17', 'false', 3, 2),
+(3, 'Avatar', 'movies', '2023/01/21', 'false', 7, 3),
+(4, 'Dish soap', 'products', '2023/01/06', 'true', 10, 4);
