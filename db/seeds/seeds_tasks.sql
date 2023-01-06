dcac1e338fd0940cd84af7154674d50a3adfc3c0
Chelsea Dwarika <chelseakristina@live.com>
2023-01-06 21:53:15 +0000

    create seed data

---
+++ db/seeds/seeds_tasks.sql
@@ -0,0 +1,5 @@
+INSERT INTO tasks (id, name, category, due_date, completed, priority, users_id) VALUES
+(1, 'Harry Potter', 'books', '14/01/2023', 'true', 5, 1),
+(2, 'Tacofino', 'restaurants', '17/02/2023', 'false', 3, 2),
+(3, 'Avatar', 'movies', '22/01/2023', 'false', 7, 3),
+(4, 'Dish soap', 'products', 'true', 10, 4);
