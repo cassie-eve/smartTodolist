163a7e4674f1d25d5cec5145474b5a3a5b4457d7
Chelsea Dwarika <chelseakristina@live.com>
2023-01-06 21:56:30 +0000

    add missing date for one seed task

--- db/seeds/seeds_tasks.sql
+++ db/seeds/seeds_tasks.sql
@@ -2,4 +2,4 @@ INSERT INTO tasks (id, name, category, due_date, completed, priority, users_id) 
 (1, 'Harry Potter', 'books', '14/01/2023', 'true', 5, 1),
 (2, 'Tacofino', 'restaurants', '17/02/2023', 'false', 3, 2),
 (3, 'Avatar', 'movies', '22/01/2023', 'false', 7, 3),
-(4, 'Dish soap', 'products', 'true', 10, 4);
+(4, 'Dish soap', 'products', '06/01/2023', 'true', 10, 4);
