99cd0f131a4311035efe3c6e5224a7509ccd6b26
Cassandra Keddis <cassandra@thinkific.com>
2023-01-08 16:29:39 -0800

    update db queries and seeds

---
+++ public/scripts/helpers.js
@@ -0,0 +1,30 @@
+let tiny = require('tiny-json-http');
+
+// categorize a string as 'To Watch', 'To Eat', 'To Read', or 'To Buy'
+const categorize = function(string) {
+  let url = 'https://api.uclassify.com/v1/cassandraeve/smarttodolist/classify';
+  let data = {"texts":[string]};
+  let headers = {
+    'Content-Type': 'application/json',
+    Authorization: 'Token YDEFqA9byG5i'
+  };
+
+  // eslint-disable-next-line func-names
+  tiny.post({url, data, headers}, function _posted(err, result) {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      let category = '';
+      let score = 0;
+      for (let name of result.body[0].classification) {
+        if (name.p > score) {
+          category = name.className;
+          score = name.p;
+        }
+      }
+      return category;
+    }
+  });
+};
+
+module.exports = { categorize };
