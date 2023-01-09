0a82c688c5d837b03546ae0e0d125fb874fcbfd0
Gaurav Mahant <thegau22@gmail.com>
2023-01-08 20:00:19 -0700

    refactor /api/widgets route to /api/tasks used for testing database connection

---
+++ routes/tasks-api.js
@@ -0,0 +1,23 @@
+/*
+ * All routes for Widget Data are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /api/widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const router = require('express').Router();
+const taskQueries = require('../db/queries/tasks');
+
+router.get('/', (req, res) => {
+  taskQueries.getTasks()
+    .then(data => {
+      res.json({ data });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+module.exports = router;
