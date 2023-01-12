f91cac742c23d63617fb0c9a12fd252e2450c7f7
Chelsea Dwarika <chelseakristina@live.com>
2023-01-12 17:51:42 +0000

    using date.format to fix date output from database

--- public/scripts/tasks.js
+++ public/scripts/tasks.js
@@ -1,11 +1,17 @@
 // Client facing scripts here
 $(document).ready(function() {
 
+const formatDate = function(due_date) {
+    return dateFormat(due_date, "isoDate");
+};
+
+
+
   const createTaskElement = function(data) {
     let $task = $(`<li class="task">
       <label for="${data.id}">
         <div id="priority">${data.priority} </div}
-        <div id="duedate">${data.due_date} </div>
+        <div id="duedate">${formatDate(data.due_date)}</div>
         <input onclick="updateStatus(this)" type="checkbox" id="${data.id}" ${data.completed}>
         <p class="${data.completed}">${data.name}</p>
         <p>${data.category}</p>
@@ -13,7 +19,7 @@ $(document).ready(function() {
       <div class="settings">
         <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
         <ul class="task-menu test">
-          <li onclick='editTask(${data.id}, "${data.name}", "${data.due_date}", "${data.priority}")'><i class="fa-solid fa-pen-to-square"></i>Edit</li>
+          <li onclick='editTask(${data.id}, "${data.name}", "${formatDate(data.due_date)}", "${data.priority}")'><i class="fa-solid fa-pen-to-square"></i>Edit</li>
           <li onclick='deleteTask(${data.id})'><i class="fa-solid fa-trash"></i>Delete</li>
         </ul>
       </div>
