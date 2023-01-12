400be051f0f73a88a806c1e9b602643c067d111d
Chelsea Dwarika <chelseakristina@live.com>
2023-01-12 19:49:03 +0000

    fix scrolling on main page to show more tasks

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,15 @@
--- Users table seeds here (Example)
+-- command to run against midterm DB
+--    \i db/seeds/01_users.sql
+
 INSERT INTO users (username, password) VALUES ('gaurav', 'password');
 INSERT INTO users (username, password) VALUES ('sheri', 'password');
 INSERT INTO users (username, password) VALUES ('chelsea', 'password');
 INSERT INTO users (username, password) VALUES ('cass', 'password');
+INSERT INTO users (username, password) VALUES ('paul', 'password');
+INSERT INTO users (username, password) VALUES ('ringo', 'password');
+INSERT INTO users (username, password) VALUES ('john', 'password');
+INSERT INTO users (username, password) VALUES ('george', 'password');
+INSERT INTO users (username, password) VALUES ('nicki', 'password');
+INSERT INTO users (username, password) VALUES ('cardi', 'password');
+INSERT INTO users (username, password) VALUES ('beyonce', 'password');
+INSERT INTO users (username, password) VALUES ('solana', 'password');
--- db/seeds/02_tasks.sql
+++ db/seeds/02_tasks.sql
@@ -1,5 +1,16 @@
+-- command to run against midterm DB
+--    \i db/seeds/02_tasks.sql
+
 INSERT INTO tasks (name, category, due_date, completed, priority, users_id) VALUES
 ('Harry Potter', 'To Read', '2023/01/14', 'true', 3, 1),
 ('Tacofino', 'To Eat', '2023/02/17', 'false', 3, 2),
 ('Avatar', 'To Watch', '2023/01/21', 'false', 2, 3),
-('Dish soap', 'To Buy', '2023/01/06', 'true', 1, 4);
+('Dish soap', 'To Buy', '2023/01/06', 'true', 1, 4),
+('Sapiens', 'To Read', '2023/01/14', 'true', 3, 1),
+('Noma', 'To Eat', '2023/02/17', 'false', 3, 2),
+('White Lotus', 'To Watch', '2023/01/21', 'false', 2, 3),
+('Duvet cover', 'To Buy', '2023/01/06', 'true', 1, 4),
+('Bon Appetit Magazine', 'To Read', '2023/01/14', 'true', 3, 1),
+('Matzo ball soup', 'To Eat', '2023/02/17', 'false', 3, 2),
+('Golden Globes Awards Show', 'To Watch', '2023/01/21', 'false', 2, 3),
+('Toothpaste', 'To Buy', '2023/01/06', 'true', 1, 4);
