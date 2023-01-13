ea1a7b57b8d6394bf4d4dfc40f5bc8e43025c3f9
Gaurav Mahant <thegau22@gmail.com>
2023-01-13 11:50:33 -0700

    add route for delete

--- routes/tasks-api.js
+++ routes/tasks-api.js
@@ -5,6 +5,22 @@
 const router = require('express').Router();
 const taskQueries = require('../db/queries/tasks');
 
+//CRUD
+//CREATE - POST /
+router.post('/', (req, res) => {
+  taskQueries.addTasks(req.body)
+    .then(data => {
+      res.json({ data });
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+//READ ALL - GET /
 router.get('/', (req, res) => {
   taskQueries.getTasks()
     .then(data => {
@@ -17,17 +33,22 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-  taskQueries.addTasks(req.body)
-    .then(data => {
-      res.json({ data });
-    })
-    .catch(err => {
-      console.log(err);
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+//READ ONE - GET /:id
+//UPDATE - POST /:id/edit
+
+
+//DELETE - POST /:id/delete
+router.post('/:id/delete', (req, res) => {
+  taskQueries.deleteTask(req.params.id)
+  .then(data => {
+    res.json({ data });
+  })
+  .catch(err => {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: err.message });
+  });
 });
 
 module.exports = router;
